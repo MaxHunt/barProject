9621c7de7aeefb0454fc47c5fa9b41991b35b6ed
Max Hunt <scarymrmoley@hotmail.co.uk>
2020-10-28 22:03:56 +0000

    purple and yellow preset

--- static/presets.js
+++ static/presets.js
@@ -124,5 +124,68 @@ const presets = [
       g: 255,
       b: 255
     }
+  },
+  {
+    name: 'Purple and Yellow',
+    shelf1: {
+      r: 227,
+      g: 19,
+      b: 242
+    },
+    shelf2: {
+      r: 242,
+      g: 242,
+      b: 19
+    },
+    shelf3: {
+      r: 227,
+      g: 19,
+      b: 242
+    },
+    shelf4: {
+      r: 242,
+      g: 242,
+      b: 19
+    },
+    shelf5: {
+      r: 242,
+      g: 242,
+      b: 19
+    },
+    shelf6: {
+      r: 227,
+      g: 19,
+      b: 242
+    },
+    shelf7: {
+      r: 242,
+      g: 242,
+      b: 19
+    },
+    shelf8: {
+      r: 227,
+      g: 19,
+      b: 242
+    },
+    floorLeft: {
+      r: 242,
+      g: 242,
+      b: 19
+    },
+    floorMiddle: {
+      r: 227,
+      g: 19,
+      b: 242
+    },
+    floorRight: {
+      r: 242,
+      g: 242,
+      b: 19
+    },
+    blackboard: {
+      r: 242,
+      g: 242,
+      b: 19
+    }
   }
 ];

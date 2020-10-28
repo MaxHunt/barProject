f8c248e34dc964606417e228d168b7c2106558b4
Sam Wooler <swooler592@gmail.com>
2020-10-28 21:02:18 +0000

    added shelf lights presets file

---
+++ static/presets.js
@@ -0,0 +1,65 @@
+const presets = [
+  {
+    name: 'default',
+    shelf1: {
+      r: 255,
+      g: 255,
+      b: 255
+    },
+    shelf2: {
+      r: 255,
+      g: 255,
+      b: 255
+    },
+    shelf3: {
+      r: 255,
+      g: 255,
+      b: 255
+    },
+    shelf4: {
+      r: 255,
+      g: 255,
+      b: 255
+    },
+    shelf5: {
+      r: 255,
+      g: 255,
+      b: 255
+    },
+    shelf6: {
+      r: 255,
+      g: 255,
+      b: 255
+    },
+    shelf7: {
+      r: 255,
+      g: 255,
+      b: 255
+    },
+    shelf8: {
+      r: 255,
+      g: 255,
+      b: 255
+    },
+    floorLeft: {
+      r: 255,
+      g: 255,
+      b: 255
+    },
+    floorMiddle: {
+      r: 255,
+      g: 255,
+      b: 255
+    },
+    floorRight: {
+      r: 255,
+      g: 255,
+      b: 255
+    },
+    blackboard: {
+      r: 255,
+      g: 255,
+      b: 255
+    }
+  }
+];

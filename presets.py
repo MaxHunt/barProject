79bf89dcb0fc3585cd5f64ac152e8d2a16e8234e
Sam Wooler <swooler592@gmail.com>
2020-11-06 19:34:26 +0000

    made path in PresetsAPI os agnostic

--- presets.py
+++ presets.py
@@ -6,7 +6,7 @@ from flask.views import MethodView
 class PresetsAPI(MethodView):
   def get(self):
     directory = os.path.dirname(os.path.abspath(__file__))
-    presets_path = os.path.join(directory, 'data\\presets.json')
+    presets_path = os.path.join(directory, 'data', 'presets.json')
     with open(presets_path) as f:
       presets_data = f.read()
     return presets_data, 200
